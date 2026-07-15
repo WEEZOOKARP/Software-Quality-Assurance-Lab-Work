b26a044fe8d32374f868da57ae7fe498be73cfc3
WEEZOOKA <76672483+WEEZOOKARP@users.noreply.github.com>
2026-07-15 12:02:57 +1200

    Initial Commit (Week 1 - Not yet functional)

---
+++ ENSE707_Week1_Lab/ENSE707_Week1_Lab/Program.cs
@@ -0,0 +1,44 @@
+﻿class Core
+{
+    BankAccount account;
+    int accounts = 0;
+    public void Main(String[] args)
+    {
+        Console.WriteLine("Banking System ---");
+        interactionScreen();
+    }
+
+    void interactionScreen()
+    {
+        String[] items = {"1 - Create account", "2 - Deposit", "3 - Withdraw"};
+
+        foreach (var item in items)
+        {
+            Console.WriteLine(item);
+        }
+    
+        String userInput = Console.ReadLine();
+
+        switch (userInput)
+        {
+            case "1":
+                accounts++;
+                account = new BankAccount(accounts);
+                break;
+            case "2":
+                Console.WriteLine("How much would you like to deposit?");
+                float value;
+                try
+                {
+                    value = Convert.ToSingle(Console.ReadLine());
+                    account.deposit(value);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error has occured.");
+                    Console.WriteLine(e.Message);
+                }
+                break;
+        };
+    }
+}
---
+++ ENSE707_Week1_Lab/ENSE707_Week1_Lab/bankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class BankAccount
+{
+	private float balance;
+    public int accountID;
+	public BankAccount(int accountIdentifier)
+	{
+		balance = 0;
+		accountID = accountIdentifier;
+	}
+
+	public float getBalance()
+	{
+        return this.getBalanceImpl();
+	}
+
+	private float getBalanceImpl()
+	{
+		return this.balance;
+	}
+
+    public void deposit(float value)
+    {
+        if (value <= 0)
+            throw new ArgumentException("You cannot deposit values less than or equal to 0");
+        
+        this.depositImpl(value);
+    }
+    private void depositImpl(float value)
+    {
+        this.balance += value;
+    }
+
+    public void withdraw(float value)
+    {
+        if (value <= 0)
+            throw new ArgumentException("You cannot withdraw values less than or equal to 0");
+
+        this.withdrawImpl(value);
+    }
+    private void withdrawImpl(float value)
+    {
+        this.balance -= value;
+    }
+}
